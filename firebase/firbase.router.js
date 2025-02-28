98c2ae833ec9bb26525ac75d7c814c9f98f52f23
Vishvampatel100 <bittupatel27@hotmail.com>
2025-02-28 13:07:45 -0600

    token auth change

--- firebase/firbase.router.js
+++ firebase/firbase.router.js
@@ -4,7 +4,12 @@ import admin from './firebaseAdmin.js';
 const firebaseRouter = express.Router();
 
 firebaseRouter.post('/api/login', async (req, res) => {
-    const { idToken } = req.body;
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) {
+        return res.status(401).json({ error: 'Sorry! No token provided' });
+    }
+    
+    const idToken = authHeader.split(' ')[1];
     try {
         const decodedToken = await admin.auth().verifyIdToken(idToken);
         const token = await admin.auth().createCustomToken(decodedToken.uid);

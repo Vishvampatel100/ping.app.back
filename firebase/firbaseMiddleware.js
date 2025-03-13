a8db586b986253989c9b0b5c4660d69b7e94542e
Vishvampatel100 <bittupatel27@hotmail.com>
2025-03-13 13:44:27 -0500

    fix attachments

--- firebase/firbaseMiddleware.js
+++ firebase/firbaseMiddleware.js
@@ -4,15 +4,18 @@ import admin from './firebaseAdmin.js';
 const verifyTokenFirebase = async (req, res, next) => {
     const authHeader = req.headers['authorization'];
     if (!authHeader) {
+        console.log('No token provided');
         return res.status(401).json({ error: 'No token provided' });
     }
     //get gearer token
+    console.log('No token provided passed');
     const token = authHeader.split(' ')[1];
     try {
         const decodedToken = await admin.auth().verifyIdToken(token);
         req.user = decodedToken;
         next();
     } catch (error) {
+        console.log(error);
         res.status(401).json({ error: 'Invalid token' });
     }
 };
